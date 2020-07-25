ec87980c0f953e7c2beaee62db1a7d692de05759
Hany <hany.rabah@gmail.com>
2020-07-25 15:42:12 +0400

    fixing Button import

--- components/layouts/BasePage.js
+++ components/layouts/BasePage.js
@@ -1,7 +1,7 @@
 import Header from "@/components/shared/Header";
 import Aside from "../shared/Aside";
 import Link from "next/link";
-import Button from 'reactstrap';
+import { Button } from 'reactstrap';
 
 const BasePage = ({ children, className = "", title, button, buttonHref }) => {
   console.log("BasePage -> buttonHref", buttonHref);
